7547510eb4028f91b8dfd568d9914a5c04cdea45
nevakee716 <nevakee716@gmail.com>
2020-01-21 14:36:29 +0100

    remove useless old config

--- src/cwHtml5DiagramFilter.js
+++ src/cwHtml5DiagramFilter.js
@@ -29,23 +29,7 @@
     cwApi.pluginManager.register("CwDiagramViewer.initWhenDomReady", this.init.bind(this));
     cwApi.pluginManager.register("CwDiagramViewer.beforeDrawShapeRegion", this.setGlobalAlphaRegion.bind(this));
     cwApi.pluginManager.register("CwDiagramViewer.afterDrawShapeRegion", this.resetGlobalAlpha.bind(this));
-    //cwApi.pluginManager.register("CwDiagramViewer.tickEnd", this.manageGPS.bind(this));
   };
-
-  function shouldDisplayJoiner(diagramViewer, joiner) {
-    var toShape, fromShape;
-
-    if (diagramViewer.isInEditMode === true) {
-      toShape = diagramViewer.getDiagramShapeBySequence(joiner.joiner.ToSeq);
-      fromShape = diagramViewer.getDiagramShapeBySequence(joiner.joiner.FromSeq);
-      if (toShape !== undefined && fromShape !== undefined && toShape !== null && fromShape !== null) {
-        if (toShape.shape.moveTo !== undefined || fromShape.shape.moveTo !== undefined) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
 
   cwApi.CwPlugins.PsgDiagramFilter = PsgDiagramFilterManager;
 
